efc5d8a8fa16da06de5d315bceefcfa54349644e
Jordan Folk <jordan.folk7@gmail.com>
2021-12-02 18:04:15 -0800

    started lab, passes 1 test

---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,39 @@
+#Band  
+    ##name
+    ##members from musician class
+    ##play_solo method that asks each member to play a solo in order they were added to the badn
+    ##appropriate __str__ and __repr__ methods
+    ##class method to_list which returns list of previous band instances
+class Band():
+    name = ''
+
+    pass
+
+class Musician():
+    pass
+
+
+class Guitarist():
+    pass
+
+class Bassist():
+    pass
+
+class Drummer():
+    pass
+
+
+
+
+
+
+
+
+
+
+#musician class defines features of a musician like instruments played
+    ##__str__ and __repr__ methods
+    ## get_instrument method that returns a string
+    ## play_solo that returns a string
+### guitarist, bassist, drummer
+

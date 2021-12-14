6f644b57aaa417972ae09d7fb6b65251d5233aae
Jordan Folk <jordan.folk7@gmail.com>
2021-12-14 12:56:21 -0800

    updated lab to include band and musician classes, lab complete

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -4,36 +4,57 @@
     ##play_solo method that asks each member to play a solo in order they were added to the badn
     ##appropriate __str__ and __repr__ methods
     ##class method to_list which returns list of previous band instances
-class Band():
-    name = ''
+class Band:
+    instances = []
 
-    pass
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
 
-class Musician():
-    pass
+    def play_solos(self):
+        solos = list(range(len(self.members)))
+        for idx, member in enumerate(self.members):
+            solos[idx] = member.play_solo()
+        
+        return solos
 
-
-class Guitarist():
-    pass
-
-class Bassist():
-    pass
-
-class Drummer():
-    pass
-
-
-
-
-
-
-
-
-
+    @classmethod
+    def to_list(cls):
+        return cls.instances
 
 #musician class defines features of a musician like instruments played
     ##__str__ and __repr__ methods
     ## get_instrument method that returns a string
     ## play_solo that returns a string
-### guitarist, bassist, drummer
 
+class Musician:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"{type(self).__name__} instance. Name = {self.name}"
+    
+    def get_instrument(self):
+        return self.instrument
+
+    def play_solo(self):
+        return self.solo
+
+
+
+### guitarist, bassist, drummer
+class Guitarist(Musician):
+    instrument = "guitar"
+    solo = "face melting guitar solo"
+
+class Bassist(Musician):
+    instrument = "bass"
+    solo = "bom bom buh bom"
+
+class Drummer(Musician):
+    instrument = "drums"
+    solo = "rattle boom crash"
